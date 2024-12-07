9e2c564ffe00991634899424e743406775c3bf17
84Yogeshdaksh <yogeshdaksh3362@gmail.com>
2024-12-07 05:45:04 -0800

    mysqli (object oriented approach

---
+++ U2MySql_01.php
@@ -0,0 +1,17 @@
+<?php
+// Object-oriented approach
+
+$servername = "localhost";
+$username = "root";   
+$password = "";            
+
+
+$conn = new mysqli($servername, $username, $password);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error); 
+} else {
+    echo "Connection successful";
+}
+?>
